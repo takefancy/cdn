b1f17f41ea7dc77de63d8a0b0f3fad2b41b3c6ef
trangsinh87 <trangsinh87@yahoo.com.vn>
2021-02-15 23:54:23 +0700

    pagination url query string

--- bo/js/site.js
+++ bo/js/site.js
@@ -21,11 +21,37 @@ $(function() {
             return decodeURIComponent(results[2].replace(/\+/g, " "));
         }
 
+        function updateUrlParameter(uri, key, value) {
+            // remove the hash part before operating on the uri
+            var i = uri.indexOf('#');
+            var hash = i === -1 ? '' : uri.substr(i);
+            uri = i === -1 ? uri : uri.substr(0, i);
+
+            var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+            var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+
+            if (!value) {
+                // remove key-value pair if value is empty
+                uri = uri.replace(new RegExp("([?&]?)" + key + "=[^&]*", "i"), '');
+                if (uri.slice(-1) === '?') {
+                    uri = uri.slice(0, -1);
+                }
+                // replace first occurrence of & by ? if no ? is present
+                if (uri.indexOf('?') === -1) uri = uri.replace(/&/, '?');
+            } else if (uri.match(re)) {
+                uri = uri.replace(re, '$1' + key + "=" + value + '$2');
+            } else {
+                uri = uri + separator + key + "=" + value;
+            }
+            return uri + hash;
+        }
+
         var goTo = function(page) {
             var current = +(getParameterByName('page') || 1);
             if (page != current) {
                 var url = window.location.href;
-                var page = url.split('?')[0] + '?page=' + page;
+                // var page = url.split('?')[0] + '?page=' + page;
+                var page = updateUrlParameter(url, 'page', page);
                 window.location.assign(page);
             }
         }
@@ -79,7 +105,7 @@ $(function() {
         var keyword = $('#txtSearch').val();
         var date = $('#txtDate').html();
         var url = utils.query('keyword', keyword);
-        if(date){
+        if (date) {
             url = utils.query('date', date, url);
         }
         window.location.assign(url);
@@ -260,8 +286,8 @@ Vue.directive('select', {
 $(function() {
     $('.daterange-picker').each(function() {
         var format = function(e) {
-                return e.format('YYYY/MM/DD');
-            };
+            return e.format('YYYY/MM/DD');
+        };
         var el = $(this);
         var update = function(start, end) {
             var span = $(el).find('span');
